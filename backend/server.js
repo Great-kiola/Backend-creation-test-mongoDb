1bcbbe00115524d989f204e40ba77513c60507a3
ola <hollabodey.kilaso@gmail.com>
2025-05-18 21:43:22 +0100

    Fix formatting and spacing in blog creation endpoint for improved readability

--- backend/server.js
+++ backend/server.js
@@ -5,25 +5,25 @@ import Blog from "./model/blog.model.js";
 
 dotenv.config();
 const port = process.env.PORT
-const app = express(); 
+const app = express();
 
 app.use(express.json())
 
-app.post("api/blog", async (req,res) => {
+app.post("api/blog", async (req, res) => {
     const blog = req.body;
 
-    if (!blog.title || !blog.author || !blog.body){
-        return res.status(400).json({success: false, message: "Please provide all fields"})
+    if (!blog.title || !blog.author || !blog.body) {
+        return res.status(400).json({ success: false, message: "Please provide all fields" })
     }
 
     const newBlog = new Blog(blog)
 
     try {
         await newBlog.save()
-        res.status(201).json({success: true, data: newproduct})
+        res.status(201).json({ success: true, data: newproduct })
     } catch (error) {
         console.log("Error in creating blog:", error.message)
-        res.status(500).json({success: false, message: "Server Error"})
+        res.status(500).json({ success: false, message: "Server Error" })
     }
 })
 
