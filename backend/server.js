cacb16606063912a6ca96794702b231a948dfaa5
ola <hollabodey.kilaso@gmail.com>
2025-05-18 21:43:04 +0100

    Refactor server.js to implement blog creation endpoint and improve error handling

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,31 @@
 import express from "express"
 import dotenv from "dotenv"
 import { connectDB } from "./config/db.js";
+import Blog from "./model/blog.model.js";
 
 dotenv.config();
-
-const app = express(); 
 const port = process.env.PORT
+const app = express(); 
 
-app.get("/", (req,res) => {
-    console.log("here")
-    res.send("hello")
+app.use(express.json())
+
+app.post("api/blog", async (req,res) => {
+    const blog = req.body;
+
+    if (!blog.title || !blog.author || !blog.body){
+        return res.status(400).json({success: false, message: "Please provide all fields"})
+    }
+
+    const newBlog = new Blog(blog)
+
+    try {
+        await newBlog.save()
+        res.status(201).json({success: true, data: newproduct})
+    } catch (error) {
+        console.log("Error in creating blog:", error.message)
+        res.status(500).json({success: false, message: "Server Error"})
+    }
 })
-
-app.get("/users", (req, res) => {
-    res.send("Users list")
-}) 
 
 app.listen(port, () => {
     connectDB();
